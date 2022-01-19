730a9c3d338920f028934dcf9086d0152e3f794e
Juliusz Trochimczuk <juliusz.trochimczuk@infotech.edu.pl>
2022-01-19 11:26:02 +0100

    Return of hangman

---
+++ Unity Games/Hangman/Assets/Script/LetterCheck.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LetterCheck : MonoBehaviour
+{
+    public InputField Literka;
+    public Button PrzyciskSprawdzania;
+
+    public Canvas KoniecGryCanvas;
+    public Text Komunikat;
+    public Button Wyjscie;
+    public Button GrajJeszczeRaz;
+
+    private bool Sprawdzanie = false;
+
+    public string WpisanaLiterka;
+    public char Sprawdzajka;
+
+    ListGenerator listGenerator;
+
+    public GameObject[] Wisielec;
+
+    private int IloscProb = 0;
+    private int IloscRazy = 14;
+    private char ZmiennaPomocnicza;
+    private int ZmiennaPomocnicza2;
+
+    public string Wygrana;
+    public string Przegrana;
+
+    void Start()
+    {
+        listGenerator = this.gameObject.GetComponent<ListGenerator>();
+        Literka = Literka.GetComponent<InputField>();
+        PrzyciskSprawdzania = PrzyciskSprawdzania.GetComponent<Button>();
+        KoniecGryCanvas = KoniecGryCanvas.GetComponent<Canvas>();
+        Komunikat = Komunikat.GetComponent<Text>();
+        Wyjscie = Wyjscie.GetComponent<Button>();
+        GrajJeszczeRaz = GrajJeszczeRaz.GetComponent<Button>();
+
+        for (int i=0; i<14; i++)
+        {
+            Wisielec[i].SetActive(false);
+        }
+
+        KoniecGryCanvas.enabled = false;
+    }
+    
+    void Update()
+    {
+        WpisanaLiterka = Literka.text;
+
+        if (IloscProb != IloscRazy)
+        {
+            if (Sprawdzanie == true)
+            {
+                Gra();
+            }
+        }
+    }
+
+    public void EnterButton()
+    {
+        if (WpisanaLiterka.Length != 1)
+        {
+            Literka.text = "";
+        }
+        else
+        {         
+            Sprawdzajka = WpisanaLiterka[0]; 
+            Sprawdzanie = true;
+        }
+    }
+
+    public void Gra()
+    {
+        for (int i = 0; i < listGenerator.WybraneSlowo.Length; i++)
+        {
+            ZmiennaPomocnicza = listGenerator.WybraneSlowo[i];
+
+            if (Sprawdzajka == ZmiennaPomocnicza)
+            {
+                listGenerator.ListaGracza[i] = WpisanaLiterka;
+                listGenerator.DlugoscSlowa = listGenerator.DlugoscSlowa - 1;
+            }
+            else
+            {
+                ZmiennaPomocnicza2 = ZmiennaPomocnicza2 + 1;
+                continue;
+            }
+        }
+        if (ZmiennaPomocnicza2 == listGenerator.WybraneSlowo.Length)
+        {
+            IloscProb = IloscProb + 1;
+            for (int z = 0; z < IloscProb; z++)
+            {
+                Wisielec[z].SetActive(true);
+            }
+        }
+        ZmiennaPomocnicza2 = 0;
+        Sprawdzanie = false;
+        Literka.text = "";
+        if (listGenerator.DlugoscSlowa == 0 || IloscProb == IloscRazy)
+        {
+            KoniecGry();
+        }
+    }
+
+    public void KoniecGry()
+    {
+        KoniecGryCanvas.enabled = true;
+
+        if (IloscProb == IloscRazy)
+        {
+            Komunikat.text = Przegrana;
+        }
+        else
+        {
+            Komunikat.text = Wygrana;
+        }
+    }
+
+    public void QuitPrzycisk()
+    {
+        Application.Quit();
+    }
+
+    public void GrajJesczeRazPrzycisk()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
---
+++ Unity Games/Hangman/Assets/Script/ListGenerator.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ListGenerator : MonoBehaviour
+{
+    public string[] ListaSlowLatwych;
+    public string[] ListaSlowTrudnych;
+    public List<string> ListaGracza = new List<string>();
+
+    DifficultyLevelChose LevelChose;
+
+    System.Random random = new System.Random();
+
+    public string WybraneSlowo;
+    public string ListaGraczaTekst;
+    public int DlugoscSlowa;
+
+    public Text DisplayList;
+
+    int Index;
+
+    public bool Gotowy = false;
+
+    void Start()
+    {
+        DisplayList = DisplayList.GetComponent<Text>();
+        LevelChose = this.gameObject.GetComponent<DifficultyLevelChose>();
+
+        Index = random.Next(0, ListaSlowLatwych.Length);
+    }
+
+   void Update()
+    {
+        if (LevelChose.Latwy == true)
+        {
+            WybraneSlowo = ListaSlowLatwych[Index];
+            Debug.Log(WybraneSlowo);
+            for (int i = 0; i < WybraneSlowo.Length; i++)
+            {
+                ListaGracza.Add("x");
+            }
+            LevelChose.Latwy = false;
+            Gotowy = true;
+            DlugoscSlowa = WybraneSlowo.Length;
+        }
+        else if (LevelChose.Trudny == true)
+        {
+            WybraneSlowo = ListaSlowTrudnych[Index];
+            Debug.Log(WybraneSlowo);
+            for (int i = 0; i < WybraneSlowo.Length; i++)
+            {
+                ListaGracza.Add("x");
+            }
+            LevelChose.Trudny = false;
+            Gotowy = true;
+            DlugoscSlowa = WybraneSlowo.Length;
+        }
+        ListaGraczaTekst = string.Join("", ListaGracza);
+
+        DisplayList.text = ListaGraczaTekst;
+    }
+}
